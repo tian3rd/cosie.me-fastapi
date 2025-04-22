802de4857a92f30cb9cb3b5e04dfdce5dddfdf3f
Tian Wu <tianwu@outlook.com>
2025-04-22 15:29:32 +1000

    Refactor main

--- main.py
+++ main.py
@@ -17,41 +17,42 @@ top = Path(__file__).resolve().parent
 
 app.mount("/static/css", StaticFiles(directory=top / "src" / "style"), name="static")
 app.mount("/static/images", StaticFiles(directory=top / "src" / "image"), name="static")
+app.mount(
+    "/static/favicon", StaticFiles(directory=top / "src" / "image" / "favicon.ico"), name="static"
+)
 
 app.include_router(blog_router)
 
-cosie_dob = datetime(2022, 6, 22)
-today = datetime.now()
-# calculate the age of Cosie in months
-cosie_age_in_month = (today.year - cosie_dob.year) * 12 + (today.month - cosie_dob.month)
-cosie_age_str = f"{cosie_age_in_month // 12} years and {cosie_age_in_month % 12} months"
+
+def get_cosie_age() -> str:
+    """Get the age of Cosie in human readable string."""
+    cosie_dob = datetime(2022, 6, 22)
+    today = datetime.now()
+    # calculate the age of Cosie in months
+    cosie_age_in_month = (today.year - cosie_dob.year) * 12 + (today.month - cosie_dob.month)
+    cosie_age_in_years, cosie_age_extra_months = cosie_age_in_month // 12, cosie_age_in_month % 12
+    # format with proper pluralization in month (Cosie is over 2 years now)
+    cosie_age_str = f"{cosie_age_in_years} years"
+    if cosie_age_extra_months == 0:
+        return cosie_age_str
+    if cosie_age_extra_months == 1:
+        return f"{cosie_age_str} and 1 month"
+    return f"{cosie_age_str} and {cosie_age_extra_months} months"
 
 
 @app.get("/")
-def root(request: Request):
+def homepage(request: Request):
+    """Just write a simple introduction about Ellen, Cosie and I."""
     self_introduction = dedent(
         f"""
-        Greetings 👋👋🐾 (also from cool Ellen 😎 and handsome Cosie/Zz, {cosie_age_str} old).
-        Ellen, Cosie and I all love hiking 🥾 (yes, we enjoy the bush capital!)
-        We are an army of three, building a better world, one line of code at a time / one bark at a time.
+        Greetings 🐾👋👋 (also from cool Ellen 😎 and handsome Cosie/Zz ⽝, {get_cosie_age()} old).
+        Cosie, Ellen and I all love hiking 🥾 (yes, we enjoy the bush capital! 🌳🪾🍃)
+        We are an army of three 🐶😎🤓, building a small world one line of code at a time / one bark at a time.
         """
     )
-
     return templates.TemplateResponse(
         "home.html", {"request": request, "self_introduction": self_introduction}
     )
-
-
-@app.get("/hi")
-def say_hi():
-    return {"message": "Hello, world!"}
-
-
-@app.get("/hi/{name}")
-def say_hi_to(name: str):
-    if name.lower() == "ellen":
-        return "Hi Ellen, I'm your boyfriend Phillip! Shall we play some tennis?"
-    return "Hi, {name}"
 
 
 if __name__ == "__main__":
