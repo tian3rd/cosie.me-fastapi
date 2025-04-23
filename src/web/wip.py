024c9b2a7ddfaee78b287f3ed43e98e6cc5c2dc9
Tian Wu <tianwu@outlook.com>
2025-04-23 14:24:55 +1000

    Experiment orbit circle effects

---
+++ src/web/wip.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="src/template")
+
+
+@router.get("/wip")
+async def wip(request: Request):
+    return templates.TemplateResponse("wip.html", {"request": request})
+
+
+@router.get("/wip/{wip_id}")
+async def wip_id(request: Request, wip_id: str):
+    return templates.TemplateResponse("wip.html", {"request": request, "wip_id": wip_id})
